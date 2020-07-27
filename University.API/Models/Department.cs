c0dbcd52818daedf5b54648c5e732e5b2483a6d0
fanis-kar <fanis.karamichalelis@gmail.com>
2020-07-27 23:51:34 +0300

    Update Project Folder Structure. Add University.API Models.

---
+++ University.API/Models/Department.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace University.API.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        public string Abbreviation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Website { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public int FacultyId { get; set; }
+
+        public Faculty Faculty { get; set; }
+    }
+}
