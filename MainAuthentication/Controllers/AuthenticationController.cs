d24b9ac57214d2aa3719a716c3a41f30abb41859
fanis-kar <fanis.karamichalelis@gmail.com>
2020-08-20 20:12:19 +0300

    Add JWT Authentication

---
+++ MainAuthentication/Controllers/AuthenticationController.cs
@@ -0,0 +1,74 @@
+﻿using MainAuthentication.Data;
+using MainAuthentication.Infrastructure;
+using MainAuthentication.Models;
+using MainAuthentication.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace JWTMicroNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ITokenBuilder _tokenBuilder;
+
+        public AuthenticationController(ApplicationDbContext context, ITokenBuilder tokenBuilder)
+        {
+            _context = context;
+            _tokenBuilder = tokenBuilder;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] Account account)
+        {
+            var dbAccount = await _context.Accounts.SingleOrDefaultAsync(u => u.UserName == account.UserName);
+
+            if (dbAccount == null)
+            {
+                return NotFound("Account not found.");
+            }
+
+            // This is just an example, made for simplicity; do not store plain passwords in the database
+            // Always hash and salt your passwords
+            var isValid = dbAccount.Password == account.Password;
+
+            if (!isValid)
+            {
+                return BadRequest("Could not authenticate account.");
+            }
+
+            var token = _tokenBuilder.BuildToken(account.UserName);
+
+            return Ok(token);
+        }
+
+        [HttpGet("verify")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> VerifyToken()
+        {
+            var username = User
+                .Claims
+                .SingleOrDefault();
+
+            if (username == null)
+            {
+                return Unauthorized();
+            }
+
+            var accountExists = await _context.Accounts.AnyAsync(u => u.UserName == username.Value);
+
+            if (!accountExists)
+            {
+                return Unauthorized();
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ MainAuthentication/Migrations/20200820161639_Init.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MainAuthentication.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Created = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                columns: new[] { "Id", "Created", "Email", "Password", "UserName" },
+                values: new object[] { 1, new DateTime(2020, 8, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), "fanis.karamichalelis@gmail.com", "1234", "fanis.kar" });
+
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                columns: new[] { "Id", "Created", "Email", "Password", "UserName" },
+                values: new object[] { 2, new DateTime(2020, 8, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), "info@uniwa.gr", "5678", "uniwa" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
